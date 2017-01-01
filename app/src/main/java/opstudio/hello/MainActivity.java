983924b7d704a269432cd11b1a2955938c1bd098
stack07142 <stack.07142@gmail.com>
2017-01-01 16:43:58 +0900

    add log

--- app/src/main/java/opstudio/hello/MainActivity.java
+++ app/src/main/java/opstudio/hello/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // log <- not added comment
         setContentView(R.layout.activity_main);
 
 
